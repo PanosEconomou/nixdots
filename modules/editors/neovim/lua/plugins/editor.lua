762cb8f9b538b289e7a9d590db96e51bb5de76d8
Panos Economou <panos.economou.v@gmail.com>
2026-06-29 13:25:08 -0400

    vim start screen

--- modules/editors/neovim/lua/plugins/editor.lua
+++ modules/editors/neovim/lua/plugins/editor.lua
@@ -14,5 +14,39 @@ return {
         opts  = {},   -- gcc to comment line, gc{motion} for range
     },
 
+    -- Customize start screen
+    {
+        "goolord/alpha-nvim",
+        event = "VimEnter",
+        config = function()
+            local alpha = require("alpha")
+            local dashboard = require("alpha.themes.dashboard")
+            dashboard.section.header.val = {
+                [[  ╻   ╻  ┏━┓┏━┓╺┳╸  ]],
+                [[  ┃   ┃  ┃ ┃┗━┓ ┃   ]],
+                [[  ╹   ┗━╸┗━┛┗━┛ ╹   ]],
+                [[       ┏┳┓╻ ╻       ]],
+                [[       ┃┃┃┗┳┛       ]],
+                [[       ╹ ╹ ╹        ]],
+                [[ ┏━╸╻ ╻┏━┓┏━┓┏━┓┏━┓ ]],
+                [[ ┃  ┃ ┃┣┳┛┗━┓┃ ┃┣┳┛ ]],
+                [[ ┗━╸┗━┛╹┗╸┗━┛┗━┛╹┗╸ ]],
+            }
+            local function get_top_padding()
+                local art_height = #dashboard.section.header.val
+                return math.max(0, math.floor((vim.fn.winheight(0) - art_height) / 2))
+            end
+            -- Layout with ONLY the header (no buttons, no footer)
+            dashboard.config.layout = {
+                { type = "padding", val = get_top_padding()},
+                dashboard.section.header,
+            }
+
+            -- Don't let the header be selectable/highlighted as a button
+            dashboard.section.header.opts.hl = "AlphaHeader"
+
+            alpha.setup(dashboard.config)
+        end,
+    }
 
 }
