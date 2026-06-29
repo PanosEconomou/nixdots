3b3c407af24b59b68ba717e0405aee8697684be9
Panos Economou <panos.economou.v@gmail.com>
2026-06-29 12:03:50 -0400

    gtk cursor fixes

--- modules/system/utilities/cursors/google_dot/default.nix
+++ modules/system/utilities/cursors/google_dot/default.nix
@@ -1,5 +1,12 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [ pkgs.google-cursor ];
+
+  # Set the gtk cursor to avoid mismatches
+  environment.etc."gtk-3.0/settings.ini".text = ''
+    [Settings]
+    gtk-cursor-theme-name=GoogleDot-White
+    gtk-cursor-theme-size=20
+  '';
 }
 
