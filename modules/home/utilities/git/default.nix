ddbcfd88983edf197ab5c60bb90806b90b97547a
Panos Economou <panos.economou.v@gmail.com>
2026-08-02 00:04:04 -0400

    git lg alias and nix develop aliases

--- modules/home/utilities/git/default.nix
+++ modules/home/utilities/git/default.nix
@@ -10,11 +10,15 @@ in
   config = lib.mkIf cfg.enable {
     programs.git = {
       enable = true;
+      lfs.enable = true;
       settings = {
         user.name = "Panos Oikonomou";
         user.email = "panos.economou.v@gmail.com";
         init.defaultBranch = "main";
         pull.rebase = true;
+        alias = {
+          lg = "log --graph --abbrev-commit --decorate --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+        };
       };
     };
   };
